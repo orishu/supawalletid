2ddb5f2c315e1600297014d3920e1697c922986d
Ori Shalev <ori.shalev@gmail.com>
2025-08-10 11:22:13 -0700

    Minor fix in the sample component (tbody)

--- src/components/SampleSupaWalletText/index.tsx
+++ src/components/SampleSupaWalletText/index.tsx
@@ -7,10 +7,12 @@ export const SampleSupaWalletText = () => {
 
     return <div>
         <table>
-            <tr><td>User ID</td><td>{supaUser?.id}</td></tr>
-            <tr><td>World username</td><td>{supaUser?.user_metadata['worldUsername']}</td></tr>
-            <tr><td>World profile pic</td><td><img width={32} src={supaUser?.user_metadata['worldProfilePictureUrl']}/></td></tr>
+            <tbody>
+                <tr><td>User ID</td><td>{supaUser?.id}</td></tr>
+                <tr><td>World username</td><td>{supaUser?.user_metadata['worldUsername']}</td></tr>
+                <tr><td>World profile pic</td><td><img width={32} src={supaUser?.user_metadata['worldProfilePictureUrl']} /></td></tr>
+            </tbody>
         </table>
-        </div>
+    </div>
 }
 
